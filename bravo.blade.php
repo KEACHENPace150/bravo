e41033054f0b1e48904acf678fe8bf48f0165c89
KEA CHEN <122611259+KEACHENPace150@users.noreply.github.com>
2023-01-21 00:17:02 +0700

    Add files via upload

---
+++ bravo.blade.php
@@ -0,0 +1,508 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LAKU CAFE </title>
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">>
+    <!- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--Bootstrap_File-Online-->
+
+            <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script> -->
+
+        <!--Bootstrap Off line-->
+        
+    <link rel="stylesheet" href="//bootstrap/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <SCript src="bootstrap/js/bootstrap.bundle.min.js"></SCript>
+
+</head>
+<body">
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo">
+    <h1>BRAVO</h1>
+    <!--<img src="images/bravo/logo.jpg" alt=""> -->
+    </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#products">products</a>
+        <a href="#review">review</a>
+        <a href="#contact">contact</a>
+        <a href="#blogs">Catagary Blogs</a>
+    </nav>
+
+    <div class="icons">
+        <div class="fas fa-search" id="search-btn"></div>
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-bars" id="menu-btn"></div> 
+    </div>
+        <div class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </div>
+    <div class="cart-items-container">
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/bravo/cdo.jpg" alt="">
+            <div class="content">
+                <h3>cart item 01</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/bravo/GTX130.jpg" alt="">
+            <div class="content">
+                <h3>cart item 02</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/bravo/logo.jpg" alt="">
+            <div class="content">
+                <h3>cart item 03</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <div class="cart-item">
+            <span class="fas fa-times"></span>
+            <img src="images/cart-item-4.png" alt="">
+            <div class="content">
+                <h3>cart item 04</h3>
+                <div class="price">$15.99/-</div>
+            </div>
+        </div>
+        <a href="#" class="btn">checkout now</a>
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+    <div class="content">
+        <h3>BRAVO REPAIRING SERVICE</h3>
+        <p>BRAVO ខេត្តកំពត់យើងខ្ញុំសូមស្វាគមន៍ <span style="font-family:Khmer Mool1">យើងខ្ញុំថ្លែងអំណរគុណយ៉ាងជ្រាវជ្រៅចំពោះការមកដល់របស់អតិថិជនទាំងអស់!!</span></p>
+        <p>BRAVO Kampot Province We truly thank all the customers who came !!</p>
+        <a href="#" class="btn">get yours now</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="images/bravo/cdo2.jpg" alt="">
+        </div>
+
+        <div class="content">
+        <h1>-------KIM HEAK QUOTE-------</h1>
+        <!--
+            <h3>what makes our coffee special?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus qui ea ullam, enim tempora ipsum fuga alias quae ratione a officiis id temporibus autem? Quod nemo facilis cupiditate. Ex, vel?</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit amet enim quod veritatis, nihil voluptas culpa! Neque consectetur obcaecati sapiente?</p>
+            <a href="#" class="btn">learn more</a>
+        </div>
+        -->
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<section class="products" id="products">
+
+    <h1 class="heading"> our <span>Customer Activitive</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/bravo/heakP.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>BRAVO REPAIRING SERVICE</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+          <div class="icons">
+              <a href="#" class="fas fa-shopping-cart"></a>
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-eye"></a>
+          </div>
+          <div class="image">
+              <img src="images/bravo/kimheak.jpg" alt="">
+          </div>
+          <div class="content">
+              <h3>BRAVO REPAIRING SERVICE</h3>
+              <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+              </div>
+          </div>
+      </div>
+
+
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/bravo/custom.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>BRAVO REPAIRING SERVICE</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+        </div>
+        <div class="box">
+          <div class="icons">
+              <a href="#" class="fas fa-shopping-cart"></a>
+              <a href="#" class="fas fa-heart"></a>
+              <a href="#" class="fas fa-eye"></a>
+          </div>
+          <div class="image">
+              <img src="images/bravo/custom1.jpg" alt="">
+          </div>
+          <div class="content">
+              <h3>BRAVO REPAIRING SERVICE</h3>
+              <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+              </div>
+          </div>
+      </div>
+
+        <div class="box">
+            <div class="icons">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <a href="#" class="fas fa-heart"></a>
+                <a href="#" class="fas fa-eye"></a>
+            </div>
+            <div class="image">
+                <img src="images/bravo/custom2.jpg" alt="">
+            </div>
+            <div class="content">
+                <h3>fresh Cookie Caramel</h3>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section starts  -->
+<!-- Start blog1 -->
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> Our Products<span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/VX2018.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>YAMAHA VX 2018</span>
+                <p>Price: 110$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/spark2019.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo Spark 2019</span>
+                <p>Price: 105$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/GTX130.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo GTX 130 Pro 2022</span>
+                <p>Price: 120$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/GTX170.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo GTX 170 2022</span>
+                <p>Price: 150$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+         <div class="box">
+            <div class="image">
+                <img src="images/bravo/RXT300.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo RXT 300 2022</span>
+                <p>Price: 150$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/VX deluxe2012.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>YAMAHA VX DELUXE 2012</span>
+                <p>Price: 75$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/GTX130.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo GTX 130 Pro 2022</span>
+                <p>Price: 120$/hour</p>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/bravo/challenger210.jpg" alt="">
+            </div>
+            <div class="content">
+                <a href="#" class="title">Product:</a>
+                <span>Seadoo Challenger 210 </span>
+                <p>Price: 450$/hour</p>
+                <span style="color:red">Capacity 10 people 1 day 1200$ with drink and food</span>
+                <a href="#" class="btn">read more</a>
+            </div>
+        </div>
+
+    </div>
+<!-- End blog1 -->
+</section>
+
+<!-- blogs section ends -->
+
+<!-- review section starts  -->
+<!--សម្រាប់ធ្វើការដាក់ឈ្មោះ សមាជិកចូលរួម -->
+
+<section class="review" id="review">
+
+    <h1 class="heading"> More <span>Information</span> </h1>
+
+    <div class="box-container">
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+            <p class="saft">
+                <li>
+                <p>Tel:+855 81 725 415</p>
+                <p><a href="#"></a></p>
+                <p>facebook:<a href="https://www.facebook.com/bee.ricky.50">https://www.facebook.com/bee.ricky.50</a></p>
+                 <p>instagram:<a href="https://instagram.com/vathanak_t124?igshid=Zjc2ZTc4Nzk=">https://instagram.com/vathanak_t124?igshid=Zjc2ZTc4Nzk=</a></p>
+                </li>
+            </p>
+            <img src="images/bravo/custom2.jpg" class="user" alt="">
+            <h3>VATHANAK</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+        
+        <div class="box">
+            <img src="images/quote-img.png" alt="" class="quote">
+           <p class="saft">
+                <li>
+                <p>Tel:+855 17 967 624</p>
+                <p><a href="#"></a></p>
+                <p>facebook:<a href="https://www.facebook.com/ly.heak.56">https://www.facebook.com/ly.heak.56</a></p>
+                 <p>instagram:<a href="https://instagram.com/jonhny_grey?igshid=Zjc2ZTc4Nzk=">https://instagram.com/jonhny_grey?igshid=Zjc2ZTc4Nzk=</a></p>
+                </li>
+            </p>
+            <img src="images/bravo/kimheak.jpg" class="user" alt="">
+            <h3>SEANG KIMHEAK</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- review section ends -->
+
+<!-- contact section starts  -->
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> <span>contact</span> us </h1>
+
+    <div class="row">
+
+        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3921.5786737222284!2d104.17776495057876!3d10.612077165069918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3108378a495fc71d%3A0x40dbd535d3461f56!2sLAKU%20Cafe%20%26%20Restaurant!5e0!3m2!1sen!2skh!4v1664784913152!5m2!1sen!2skh" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></iframe>
+
+        <form action="">
+            <h3>get in touch</h3>
+            <div class="inputBox">
+                <span class="fas fa-user"></span>
+                <input type="text" placeholder="name">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-envelope"></span>
+                <input type="email" placeholder="email">
+            </div>
+            <div class="inputBox">
+                <span class="fas fa-phone"></span>
+                <input type="number" placeholder="number">
+            </div>
+            <input type="submit" value="contact now" class="btn">
+        </form>
+
+    </div>
+
+</section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="https://www.facebook.com/profile.php?id=100084111646126" class="fab fa-facebook-f"></a>
+        <a href="#" class="fab fa-twitter"></a>
+        <a href="#" class="fab fa-instagram"></a>
+        <a href="#" class="fab fa-linkedin"></a>
+        <a href="#" class="fab fa-pinterest"></a>
+    </div>
+
+    <div class="links">
+        <a href="#">home</a>
+        <a href="#">about</a>
+        <a href="#">menu</a>
+        <a href="#">products</a>
+        <a href="#">review</a>
+        <a href="#">contact</a>
+        <a href="#">blogs</a>
+    </div>
+
+    <div class="credit" style="background-color:yellow">Created by <span>BlackLife</span> |Coppyrights <ui>BRAVO</u> </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+<!-- custom js file link  
+<script src="codejava/code.js"></script>
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+-->
+
+</body>
+</html>
